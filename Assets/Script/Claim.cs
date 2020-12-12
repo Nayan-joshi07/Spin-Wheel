8050707b89e2720e3de490f8f0166f46244ad5ae
Nayan Joshi <nayan07joshi@gmail.com>
2020-12-12 20:15:53 +0530

    Initial Commit

---
+++ Assets/Script/Claim.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Claim : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textBox;
+    [SerializeField] TextMeshProUGUI scoreBox;
+    string[] score = {"1000","1500","2000","2500","3000","3500","4000","4500","5000","5500","6000","6500"};
+    int scoreText;
+    public SpinWheel spinWheel;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ClaimText();
+    }
+    private void ClaimText()
+    {
+        scoreText = spinWheel.angle;
+        switch (scoreText)
+        {
+            case 0:
+                textBox.text = score[0];
+                break;
+            case 1:
+                textBox.text = score[1];
+                break;
+            case 2:
+                textBox.text = score[2];
+                break;
+            case 3:
+                textBox.text = score[3];
+                break;
+            case 4:
+                textBox.text = score[4];
+                break;
+            case 5:
+                textBox.text = score[5];
+                break;
+            case 6:
+                textBox.text = score[6];
+                break;
+            case 7:
+                textBox.text = score[7];
+                break;
+            case 8:
+                textBox.text = score[8];
+                break;
+            case 9:
+                textBox.text = score[9];
+                break;
+            case 10:
+                textBox.text = score[10];
+                break;
+            case 11:
+                textBox.text = score[11];
+                break;
+            case 12:
+                textBox.text = score[12];
+                break;
+        }
+    }
+    public void ClaimButton()
+    {
+        int totalScore = int.Parse(scoreBox.text);
+        totalScore += int.Parse(score[scoreText]);
+        scoreBox.text = totalScore.ToString();
+        spinWheel.ClaimToWheel();
+    }
+}
---
+++ Assets/Script/SpinWheel.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpinWheel : MonoBehaviour
+{
+    [SerializeField] Button spinButton;
+    [SerializeField] TextMeshProUGUI[] text;
+    [SerializeField] bool blowUpDown = true;
+    float randomValue;
+    float timeInterval;
+    float timeIntervalText=3.0f;
+    int textSize = 32;
+    //Set Active/Deactive the GameObject
+    public GameObject Wheel;
+    public GameObject Claim;
+    public int angle;
+
+
+    void Awake()
+    {
+    }
+    void Start()
+    {
+        StartCoroutine(WheelSpin());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if (!blowUpDown)
+        {
+            StopCoroutine(SpinButtonUpDown());
+        }
+        else
+        {
+            StartCoroutine(SpinButtonUpDown());
+        }
+
+    }
+    IEnumerator WheelSpin()
+    {
+        blowUpDown = false;
+        randomValue = Random.Range(20.0f, 30.0f);
+        timeInterval = 0.1f;
+        for (float i = 0.0f; i <= randomValue; i++)
+        {
+            transform.Rotate(0, 0, 30);
+            if (i > (randomValue * 0.5f))
+            {
+                timeInterval = 0.2f;
+            }
+            if (i > (randomValue * 0.85f))
+            {
+                timeInterval = 0.4f;
+            }
+            yield return new WaitForSeconds(timeInterval);
+        }
+        angle = (int)transform.eulerAngles.z / 30;
+        TextSystem();
+    }
+    private void TextSystem()
+    {
+        //Stop the Couritine of the Wheel;
+        StopCoroutine(WheelSpin());
+        //Start the Couritine of the Text;
+        StartCoroutine(TextBlow());
+    }
+    IEnumerator TextBlow()
+    {
+        float interval=0;
+        while (interval < timeIntervalText)
+        {
+            text[angle].fontSize = textSize;
+            interval += Time.deltaTime;
+            text[angle].fontSize = 2*textSize;
+        }
+        yield return new WaitForSeconds(timeIntervalText);
+        text[angle].fontSize = 2*textSize;
+        WheelToClaim();
+    }
+    private void WheelToClaim()
+    {
+        StopCoroutine(TextBlow());
+        Wheel.SetActive(false);
+        Claim.SetActive(true);
+    }
+    public void ClaimToWheel()
+    {
+        Claim.SetActive(false);
+        Wheel.SetActive(true);
+        blowUpDown = true;
+    }
+    public void SpinAgain()
+    {
+        if (blowUpDown)
+        {
+            StopAllCoroutines();
+            spinButton.transform.localScale = new Vector3(1f, 1f, 1f);
+            StartCoroutine(WheelSpin());
+        }
+    }
+    IEnumerator SpinButtonUpDown()
+    {
+        float timeInterval = 2.0f;
+        Vector3 temp;
+        temp = spinButton.transform.localScale;
+        for (float i = 0.0f; i < 0.1f; i = i + 0.1f)
+        {
+            temp.x += Time.deltaTime;
+            temp.y += Time.deltaTime;
+            temp.z += Time.deltaTime;
+            if (temp.x <= 1.3f)
+            {
+                spinButton.transform.localScale = temp;
+            }
+            else 
+            {
+                break;
+            }
+        }
+        yield return new WaitForSeconds(timeInterval);
+        for (float i = 0.0f; i < 0.1f; i = i + 0.1f)
+        {
+            temp.x -= Time.deltaTime;
+            temp.y -= Time.deltaTime;
+            temp.z -= Time.deltaTime;
+            if (temp.x >= 1f)
+            {
+                spinButton.transform.localScale = temp;
+            }
+            else 
+            {
+                break;
+            }
+        }
+
+    }
+}
